e3360956e4fb82eabe631e3eedf205d2ec6f68bc
NomadDsgns <nomaddsgns@gmail.com>
2024-04-16 13:10:02 -0500

    Stylized/Modified for portfolio

--- components/Stepper/Stepper.js
+++ components/Stepper/Stepper.js
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
-import './styles/Stepper.css'; // Import your CSS file for styling
+import React, { useState } from "react";
+import "./Stepper.css";
 
 const Stepper = ({ steps }) => {
-  // State to track the current step
   const [currentStep, setCurrentStep] = useState(0);
 
-  // Function to go to a specific step
   const goToStep = (step) => {
     if (step >= 0 && step < steps.length) {
       setCurrentStep(step);
     }
   };
 
-  // Function to go to the next step
   const nextStep = () => {
     goToStep(currentStep + 1);
   };
 
-  // Function to go to the previous step
   const previousStep = () => {
     goToStep(currentStep - 1);
   };
@@ -31,14 +27,14 @@ const Stepper = ({ steps }) => {
       <div className="step-navigation">
         {/* Show "Previous" button if not on the first step */}
         {currentStep > 0 && (
-          <button className="step-button" onClick={previousStep}>
+          <button className="button step-button" onClick={previousStep}>
             Previous
           </button>
         )}
 
         {/* Show "Next" button if not on the last step */}
         {currentStep < steps.length - 1 && (
-          <button className="step-button" onClick={nextStep}>
+          <button className="button step-button" onClick={nextStep}>
             Next
           </button>
         )}
